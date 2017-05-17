2d20a69ed46f450a2a584acb06f5f80e8b753635
dmexpat <expat@digitalmesh.com>
2017-05-17 20:05:37 +0530

    orm files added

---
+++ models/orm/Domain.js
@@ -0,0 +1,26 @@
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('dmcoderepo', 'root', 'pass');
+
+var Domain = sequelize.define('domain', {
+	domain_id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		allowNull: false,
+		autoIncrement: true
+	},
+	domain: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	description: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+
+}, {
+	timestamps: false,
+	freezeTableName: true
+});
+
+module.exports = Domain;
---
+++ models/orm/User.js
@@ -0,0 +1,66 @@
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('dmcoderepo', 'root', 'pass');
+
+var User = sequelize.define('user', {
+	user_id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	first_name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	last_name: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	password_reset_token: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	dob: {
+		type: Sequelize.DATEONLY,
+		allowNull: true
+	},
+	phone: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	emp_code: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	doj: {
+		type: Sequelize.DATEONLY,
+		allowNull: true
+	},
+	status: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+		defaultValue: 1
+	},
+	auth_key: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	profile_pic: {
+		type: Sequelize.STRING,
+		allowNull: true
+	}
+}, {
+	timestamps: false,
+	freezeTableName: true
+});
+
+
+module.exports = User;
